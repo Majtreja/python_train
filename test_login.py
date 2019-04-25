9fc0e3867745badacf98f8aded25b8f4498d1cc7
Majtreja <d.bielov@gmail.com>
2019-04-25 15:58:30 +0300

    fixture added

--- test_login.py
+++ test_login.py
@@ -1,39 +1,16 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
-import unittest
+import pytest
 from data import Data
+from application import Application
 
 
-class TestLoginBO(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-#        webdriver.Chrome(ChromeDriverManager().install())
-        self.driver.implicitly_wait(30)
-
-    def open_page(self):
-        driver = self.driver
-        driver.get("https://bo-qa-03.icecat.biz/home/login/")
-
-    def fill_form(self, data):
-        driver = self.driver
-        driver.find_element_by_name("Login").click()
-        driver.find_element_by_name("Login").clear()
-        driver.find_element_by_name("Login").send_keys(data.user)
-        driver.find_element_by_name("Password").click()
-        driver.find_element_by_name("Password").clear()
-        driver.find_element_by_name("Password").send_keys(data.password)
-        driver.find_element_by_xpath(
-            "(.//*[normalize-space(text()) and normalize-space(.)='REGISTER (FREE)'])[1]/preceding::button[1]").click()
-
-    def test_login(self):
-        self.open_page()
-        self.fill_form(Data(user="QA_DB_superuser", password="roo6Piv2"))
-
-    def tearDown(self):
-        self.driver.quit()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_login(app):
+    app.open_page()
+    app.fill_form(Data(user="QA_DB_superuser", password="roo6Piv2"))
